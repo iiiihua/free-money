ada284b6648bcb808d6ebf0dfd02bc6aa997702d
iiiihua <iiiihua@163.com>
2020-10-23 21:21:13 +0800

    完成基本结构（后期测试优化）

---
+++ src/views/Labels.vue
@@ -0,0 +1,74 @@
+<template>
+    <layout>
+        <div class="tags">
+            <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
+                <span>{{tag.name}}</span>
+                <Icon name="right"/>
+            </router-link>
+        </div>
+        <div class="createTag-wrapper">
+            <Button class="createTag" @click.native="createTag">新建标签</Button>
+        </div>
+    </layout>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    import Button from '@/components/Button.vue';
+    import {mixins} from 'vue-class-component';
+    import TagHelper from '@/mixins/TagHelper';
+
+    @Component({
+        components: {Button},
+    })
+    export default class Labels extends mixins(TagHelper) {
+        get tags() {
+            return this.$store.state.tagList;
+        }
+
+        beforeCreate() {
+            this.$store.commit('fetchTags');
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .tags {
+        background: white;
+        font-size: 16px;
+        padding-left: 16px;
+        max-height: 600px;
+        overflow: auto;
+
+        > .tag {
+            min-height: 44px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 1px solid #e6e6e6;
+
+            svg {
+                width: 18px;
+                height: 18px;
+                color: #666;
+                margin-right: 16px;
+            }
+        }
+    }
+
+    .createTag {
+        background: #767676;
+        color: white;
+        border-radius: 4px;
+        border: none;
+        height: 40px;
+        padding: 0 16px;
+
+        &-wrapper {
+            text-align: center;
+            padding: 16px;
+            margin-top: 28px;
+        }
+    }
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <router-link to="/">当前输入的路径不存在，点击当前链接返回主页面</router-link>
+</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
