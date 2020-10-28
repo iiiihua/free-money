d77dfff6a9760b04d8f1087255f94f6e94bdfe10
iiiihua <iiiihua@163.com>
2020-10-28 18:36:14 +0800

    优化

--- src/components/Money/Chart.vue
+++ src/components/Money/Chart.vue
@@ -12,7 +12,7 @@
     @Component
     export default class Chart extends Vue {
         @Prop() options?: EChartOption;
-        chart?: ECharts
+        chart?: ECharts;
 
         mounted() {
             if (this.options === undefined) {
@@ -21,9 +21,10 @@
             this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
             this.chart.setOption(this.options);
         }
+
         @Watch('options')
-        onOptionsChange(newValue: EChartOption){
-            this.chart!.setOption(newValue)
+        onOptionsChange(newValue: EChartOption) {
+            this.chart!.setOption(newValue);
         }
 
     }
