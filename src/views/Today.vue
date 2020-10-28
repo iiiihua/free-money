4c53ad852ce119ee7bf097468124b4501426117d
iiiihua <iiiihua@163.com>
2020-10-28 13:26:23 +0800

    优化主页

--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,6 +1,30 @@
 <template>
     <Layout>
         <h1>今日流水</h1>
+        <div class="main-container">
+            <div class="window">
+                <div class="moon"></div>
+                <div class="star st1"></div>
+                <div class="star st2"></div>
+                <div class="star st3"></div>
+                <div class="star st4"></div>
+                <div class="star st5"></div>
+                <div class="star st6"></div>
+                <div class="central-dash"></div>
+                <div class="bottom-dash"></div>
+                <div class="body"></div>
+                <div class="butt"></div>
+                <div class="ear-left"></div>
+                <div class="ear-right"></div>
+                <div class="tail"></div>
+                <div class="mt1"></div>
+                <div class="mount mt2"></div>
+                <div class="mount mt3"></div>
+                <div class="mount mt4"></div>
+                <div class="mt5"></div>
+                <div class="mount mt6"></div>
+            </div>
+        </div>
         <div class="todayOut"><span>今日支出</span>
             <div>{{getmoney}}</div>
         </div>
@@ -10,7 +34,6 @@
         <div class="readNote">
             <router-link to="/today/money">记一笔哈</router-link>
         </div>
-
     </Layout>
 </template>
 
@@ -56,7 +79,7 @@
 
     .todayOut {
         position: absolute;
-        top: 40%;
+        top: 28%;
         left: 50%;
         transform: translateX(-50%);
 
@@ -80,9 +103,8 @@
 
     .todayIn {
         position: absolute;
-        top: 55%;
+        top: 45%;
         left: 50%;
-        transform: translateX(-50%);
 
         > span {
             min-width: 100px;
@@ -95,7 +117,7 @@
 
         > div {
             padding: 10px;
-            font-size: 50px;
+            font-size: 40px;
             color: #9900ff;
         }
 
@@ -111,6 +133,264 @@
         font-size: 20px;
         background: transparent;
 
+    }
+
+    html {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    *, *:after, *:before {
+        box-sizing: inherit;
+    }
+
+    body {
+        background: #C9533E;
+    }
+
+    .window {
+        background: #29467A;
+        outline: 12px solid #2D2623;
+        border: solid #439ABF;
+        border-width: 0px 5px;
+        position: relative;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        margin: 6em auto 1em;
+        width: 260px;
+        height: 390px;
+    }
+
+    .moon-container {
+        position: relative;
+        top: 2em;
+        left: 2em;
+    }
+
+    .moon {
+        width: 80px;
+        height: 80px;
+        background: transparent;
+        border-radius: 50px;
+        box-shadow: -15px 8px 0 1px #e8ae4a;
+        position: absolute;
+        left: 3em;
+        top: 2em;
+    }
+
+    .star {
+        width: 7px;
+        height: 7px;
+        border-radius: 50px;
+        background: #E8AE4A;
+        position: absolute;
+    }
+
+    @keyframes twinkle {
+        50% {
+            box-shadow: 0 0 15px 3px rgba(232, 174, 74, 0.5);
+        }
+    }
+
+    .st1 {
+        bottom: 230px;
+        left: 30px;
+        animation: twinkle 2s infinite;
+        animation-delay: 11s;
+    }
+
+    .st2 {
+        bottom: 145px;
+        left: 150px;
+        animation: twinkle 2s infinite;
+        animation-delay: 5s;
+    }
+
+    .st3 {
+        bottom: 120px;
+        left: 40px;
+        animation: twinkle 2s infinite;
+        animation-delay: 6s;
+    }
+
+    .st4 {
+        bottom: 350px;
+        left: 220px;
+        animation: twinkle 2s infinite;
+        animation-delay: 7s;
+    }
+
+    .st5 {
+        bottom: 340px;
+        left: 120px;
+        animation: twinkle 2s infinite;
+        animation-delay: 8s;
+    }
+
+    .st6 {
+        bottom: 235px;
+        left: 200px;
+        animation: twinkle 2s infinite;
+        animation-delay: 2s;
+    }
+
+    .central-dash {
+        width: 280px;
+        height: 12px;
+        background: #2D2623;
+        position: absolute;
+        bottom: 0;
+        top: 0;
+        margin: auto -1em;
+    }
+
+    .central-dash::before {
+        content: ' ';
+        width: 250px;
+        height: 5px;
+        background: #439ABF;
+        position: absolute;
+        bottom: 100%;
+        left: 1em;
+    }
+
+    .bottom-dash {
+        width: 316px;
+        height: 12px;
+        background: #2D2623;
+        position: absolute;
+        top: 100%;
+        margin: 0 -2em;
+        z-index: 1;
+    }
+
+    .bottom-dash::before, .bottom-dash::after {
+        content: ' ';
+        width: 316px;
+        height: 4px;
+        background: #29467A;
+        position: absolute;
+        bottom: 100%;
+        z-index: 1;
+    }
+
+    .bottom-dash::after {
+        background: #439ABF;
+        width: 285px;
+        left: 15px;
+    }
+
+    .body {
+        height: 125px;
+        width: 50px;
+        border-radius: 40px 40px 0 0;
+        background: #2D2623;
+        position: absolute;
+        bottom: 0;
+        left: 35%;
+        z-index: 1;
+    }
+
+    .butt {
+        background: #2D2623;
+        height: 80px;
+        width: 40px;
+        border-radius: 0 80px 80px 0;
+        position: absolute;
+        bottom: -3%;
+        left: 53%;
+        z-index: 1;
+    }
+
+    .ear-left {
+        height: 0;
+        width: 0;
+        border-bottom: 40px solid #2d2623;
+        border-right: 33px solid transparent;
+        position: absolute;
+        bottom: 100px;
+        left: 88px;
+    }
+
+    .ear-right {
+        height: 0;
+        width: 0;
+        border-bottom: 40px solid #2d2623;
+        border-left: 33px solid transparent;
+        position: absolute;
+        bottom: 100px;
+        left: 105px;
+    }
+
+    .tail {
+        background-color: #2D2623;
+        height: 80px;
+        width: 10px;
+        border-radius: 5px;
+        position: absolute;
+        top: 100%;
+        left: 58%;
+        transform-origin: top;
+        transform: rotate(-15deg);
+        animation: tail 4s infinite;
+    }
+
+    @keyframes tail {
+        50% {
+            transform: rotate(10deg);
+        }
+    }
+
+    .mount {
+        height: 0;
+        width: 0;
+        border-bottom: 60px solid #22366D;
+        border-right: 20px solid transparent;
+        border-left: 20px solid transparent;
+        position: absolute;
+        bottom: 0;
+    }
+
+    .mt1 {
+        height: 0;
+        width: 0;
+        border-bottom: 60px solid #22366D;
+        border-right: 20px solid transparent;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }
+
+    .mt2 {
+        left: 2.5%;
+        bottom: -.7em;
+        z-index: 0;
+    }
+
+    .mt3 {
+        left: 35px;
+    }
+
+    .mt4 {
+        left: 78%;
+    }
+
+    .mt5 {
+        height: 0;
+        width: 0;
+        border-bottom: 60px solid #22366D;
+        border-left: 20px solid transparent;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+
+    .mt6 {
+        left: 65%;
+        bottom: -.7em;
     }
 
 
