bbfc680aa735164015a754bd45649ab0393148d0
iiiihua <iiiihua@163.com>
2020-11-02 14:42:18 +0800

    优化细节

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -234,8 +234,6 @@
             }
             result.map(group => {
                 group.total = group.items.reduce((sum, item) => {
-                    console.log(sum);
-                    console.log(item);
                     return sum + item.amount;
                 }, 0);
             });
